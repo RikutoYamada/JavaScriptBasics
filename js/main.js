e09fbca0978d6a56732cec96ae8ea9715460006b
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-19 07:58:20 +0900

    Add main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+// コメント
+// console.log('Hello world!');
+// console.log("Hello world!");
+
+// console.log('it's me!');
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+console.log('hello\n\tworld!')
+
+console.log('hello' + 'world')
