d97482aed2cf8ae7bf7f9934322e6130ea0a95e5
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-19 23:04:36 +0900

    Edit main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 'use strict';
-// コメント
-// console.log('Hello world!');
-// console.log("Hello world!");
 
-// console.log('it's me!');
-// console.log("it's me!");
-// console.log('it\'s me!');
-
-console.log('hello\n\tworld!')
-
-console.log('hello' + 'world')
+const signal = '';
+switch (signal) {
+  case 'red':
+    console.log('stop');
+    break;
+  case 'yellow':
+    console.log('caution');
+    break;
+  case 'blue':
+    console.log('go');
+    break;
+  default:
+    console.log('wrong signal');
+    break;
+}
