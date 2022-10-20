22bbfebb684b14c69b8a0dc655e60751d0361fbe
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-20 22:22:54 +0900

    scriptタグを分けてもスコープは分かれない

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
+{
 'use strict';
 
-const signal = '';
-switch (signal) {
-  case 'red':
-    console.log('stop');
-    break;
-  case 'yellow':
-    console.log('caution');
-    break;
-  case 'blue':
-    console.log('go');
-    break;
-  default:
-    console.log('wrong signal');
-    break;
+const x = 100;
+console.log(x);
 }
