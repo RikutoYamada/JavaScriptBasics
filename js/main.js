a1324d30f1c605db9bc7a363fdabfb372cd4d618
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-23 14:05:43 +0900

    static method thisは使えない

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 'use strict';
 {
-  const point = {
-    x: 100, 
-    y: 180,
-  };
-  // const keys = Object.keys(point);
-  // keys.forEach(key => {
-  //   console.log(`Key: ${key} Value: ${point[key]}`);
-  // });
-  const points = [
-    {x: 30, y: 20},
-    {x: 10, y: 50},
-    {x: 40, y: 40},
-  ];
-  console.log(points[1].y);
+  class Post {
+    constructor(text) {
+      this.text = text; 
+      this.likeCount = 0;
+    }
+    show() {
+      console.log(`${this.text} - ${this.likeCount}likes`);
+    }
+    
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+    
+    // this使えない
+    static showInfo() {
+      console.log('Post');
+    }
+  }
+
+  const posts = [
+    new Post('stding JavaScript'),
+    new Post('programing'),
+  ]
+  // posts[0].like();
+  // posts[0].show();
+  // posts[1].show();
+  Post.showInfo();
 }
